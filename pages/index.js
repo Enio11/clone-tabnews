42f7439332faae37fdd10cd19a87dad8a8875dd8
Enio Macedo <68479084+Enio11@users.noreply.github.com>
2025-11-17 19:53:37 +0000

    mover o `.env` para `.env.development`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Eu te amo Vitória Dias Guerreiro Pinto 🤠</h1>;
+  return (
+    <div>
+      <h1>Eu te amo Vitória Dias Guerreiro Pinto </h1>
+      <img
+        src="https://raichu-uploads.s3.amazonaws.com/logo_capitao-clean_aqLzrX.png"
+        alt=""
+        width={500}
+      />
+    </div>
+  );
 }
 
 export default Home;
