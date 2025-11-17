6bc07cde371afee756bbe8b081b95cc2be037184
Enio Macedo <enio.macedo@hotmail.com>
2025-11-17 15:18:48 -0300

    add banco de dados local

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
 test("GET to /api/v1/status returns status 200 and correct body", async () => {
-  const response = await fetch("http://localhost:3003/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
