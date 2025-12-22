20e02436adc9967b0e2fe38485948013074ee8cd
Enio Macedo <68479084+Enio11@users.noreply.github.com>
2025-12-22 17:39:50 +0000

    ssl conections terciario ssl

--- infra/database.js
+++ infra/database.js
@@ -8,8 +8,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("Credenciais do Posgres", {
+  console.log("Credenciais do Postgres", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
